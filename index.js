dc4559c9207c9d863308a37005c3f80316d35584
Justin Jeffries <justincyjeffries@gmail.com>
2021-12-14 22:48:12 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,58 @@
+function add(){
+    1 + 80
+}
+function subtract(){
+    60 - 40
+}
+function multiply(){
+    2 * 3.4
+}
+
+function divide(){
+    5.0 / 2.5
+}
+
+function add(a, b){
+ let c= a + b
+ return c
+}
+
+function subtract(a, b){
+    let c= a - b
+    return c
+   }
+
+   function multiply(a, b){
+    let c= a * b
+    return c
+   }
+
+   function divide(a, b){
+    let c= a / b
+    return c
+   }
+
+   function increment(n){
+       ++n;
+       return n
+   }
+
+   function decrement(n){
+       --n;
+       return n
+   }
+
+   function makeInt(n){
+    return parseInt(n, 10);
+      
+       
+   }
+  
+   
+
+   function preserveDecimal(n){
+    return parseFloat(n);
+    
+   }
+   
+
